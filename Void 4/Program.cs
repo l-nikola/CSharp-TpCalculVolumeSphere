8889b9dd26a8b8a7027872d43294b7298890c7d6
Nikola Legay <147250203+l-nikola@users.noreply.github.com>
2023-11-29 18:23:18 +0100

    CSharp-TpCalculVolumeSphere

---
+++ Void 4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Void_4
+{
+    class Program
+    {
+        public static double sphere(double R)
+        {
+            double V;
+            V = (double)4 / 3 * Math.PI * Math.Pow(R, 3);
+
+            return V;
+        }
+        static void Main(string[] args)
+        {
+            double R, V;
+
+            Console.WriteLine("Quel est le rayon de votre sphere ?");
+            R = Double.Parse(Console.ReadLine());
+
+            V = sphere(R);
+
+            Console.WriteLine("Le volume de votre sphere est de " + V);
+            Console.ReadLine();
+        }
+    }
+}
